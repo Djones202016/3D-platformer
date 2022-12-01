8a7c60c41794b63ffecffff2d801297cfac6ee14
Djones202016 <113114336+Djones202016@users.noreply.github.com>
2022-12-01 09:48:13 +0000

    CODE AND DEATHBOX ADDED

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBox : MonoBehaviour
+{
+   private void OnTriggerEnter(Collider private void OnWizardOtherButton() {
+       
+   }
+}
